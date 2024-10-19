4620968c6984436688d38e2f65f2180253b0a183
Elvis Rodrigo  Condori Vasquez <ec7902812@gmail.com>
2024-10-19 10:17:42 -0400

    5 en 5

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import viteLogo from '/vite.svg'
+import { Button } from './components/Button'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>AQUI MI CONTADOR DE 5 EN 5</h1>
+      <Button/>
+    </>
+  )
+}
+
+export default App
